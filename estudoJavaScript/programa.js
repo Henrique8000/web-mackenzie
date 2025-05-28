7e19eda57c9126ad876e4ed045e828943a742df0
Henrique <henriqueflavioguimaraes@gmail.com>
2025-05-27 23:16:36 -0300

    estudando java script

---
+++ estudoJavaScript/programa.js
@@ -0,0 +1,15 @@
+function msg() {
+    document.getElementById("coisa").innerHTML = "Olá";
+}
+
+
+function contador() {
+    var n = 0;
+    n += 1;
+    document.getElementById("conta").innerHTML = n;
+}
+
+
+function alerta() {
+    window.alert("EITA!")
+}
